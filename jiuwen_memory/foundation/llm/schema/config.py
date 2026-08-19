86279b343ce98d66b670d906fa03573e8f6d4a70
Hope <chenweida2@huawei.com>
2026-08-19 17:09:02 +0800

    fix: 公司名称脱敏，将ICBC替换为IntelliRouter

--- jiuwen_memory/foundation/llm/schema/config.py
+++ jiuwen_memory/foundation/llm/schema/config.py
@@ -28,7 +28,7 @@ class ModelClientConfig(BaseModel):
     client_provider: Union[ProviderType, str] = Field(
         ...,
         description="Service provider identification, Enumeration value: OpenAI, OpenRouter, "
-                    "SiliconFlow, DashScope, InferenceAffinity or ICBC"
+                    "SiliconFlow, DashScope, InferenceAffinity or IntelliRouter"
     )
     api_key: str = Field(..., description="API key")
     api_base: str = Field(..., description="API base URL")
